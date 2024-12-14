543abd2a079d4a689f00ce76f597783fcd58238d
hexaredecimal <mfanakagama@gmail.com>
2024-12-14 19:50:12 +0200

    Config: Adds a new class for configuring the generation of the template code

---
+++ src/jtempl/Config.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jtempl;
+
+/**
+ *
+ * @author user
+ */
+public class Config {
+
+	private String package_name = null, out_path = null;
+	private String input_path;
+	private String[] args;
+
+	public String getPackage_name() {
+		return package_name;
+	}
+
+	public Config(String[] args) {
+		this.args = args;
+	}
+
+	public String getOut_path() {
+		return out_path;
+	}
+
+	public String getInput_path() {
+		return input_path;
+	}
+
+	public void parse() {
+		boolean expect_package = false;
+		boolean expect_dir = false;
+		for (int i = 0; i < args.length - 1; i++) {
+			String top = args[i];
+			if (top.equals("-package")) {
+				expect_package = true;
+				continue;
+			} else if (top.equals("-dir")) {
+				expect_dir = true;
+				continue;
+			} else if (top.equals("-help")) {
+				JTempl.printUsage();
+				return;
+			}
+
+			if (expect_package) {
+				package_name = args[i];
+				expect_package = false;
+			} else if (expect_dir) {
+				out_path = args[i];
+				expect_dir = false;
+			}
+		}
+		String last = args[args.length - 1];
+		if (expect_dir) {
+			System.out.println("-dir was specified by no input is provided. Last os args is " + last);
+			System.exit(1);
+		}
+		if (expect_package) {
+			System.out.println("-package was specified by no input is provided. Last of args is " + last);
+			System.exit(1);
+		}
+		if (!last.startsWith("-"))
+			this.input_path = last;
+	}
+}
