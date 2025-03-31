ef141ff7673ad1e00549e477b1350e413a654524
hexaredecimal <mfanakagama@gmail.com>
2025-03-31 17:43:44 +0200

    HtmlTextNodeAst: Revert back to string usage

--- src/engine/ast/HtmlTextNodeAst.java
+++ src/engine/ast/HtmlTextNodeAst.java
@@ -5,15 +5,16 @@ package engine.ast;
  * @author hexaredecimal
  */
 public class HtmlTextNodeAst implements Ast {
+
 	private String text;
 
 	public HtmlTextNodeAst(String text) {
-		this.text = text.replace("\"", "\\\"");;
+		this.text = text.contains("\"") ? text.replace("\"", "\\\'").trim() : text;
 	}
 
 	@Override
 	public String exec() {
-		return String.format("sb.append(\"\"\"\n%s\n\"\"\");", this.text ).indent(4);
+		return String.format("sb.append(\"\"\"\n%s\"\"\");", this.text).indent(4);
 	}
 
 	@Override
